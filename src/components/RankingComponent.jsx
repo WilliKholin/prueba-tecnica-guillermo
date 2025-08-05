70183c6c737a2c90d6beadd7aac8c7abbb23bc06
Willi <47356997+Willicornio@users.noreply.github.com>
2025-08-05 08:18:19 +0200

    added ranking logic and layout

---
+++ src/components/RankingComponent.jsx
@@ -0,0 +1,14 @@
+import { RankingItem } from "./RankingItem";
+
+export const RankingComponent = ({ ranking }) => {
+  return (
+    <div className="bg-gray-100 p-4 rounded shadow-md">
+      <h2 className="text-xl font-semibold mb-2 text-center">Ranking - max points obtained</h2>
+      <ul className="space-y-1">
+        {ranking.map((user, index) => (
+        <RankingItem user={user} index={index}/>
+        ))}
+      </ul>
+    </div>
+  );
+};
